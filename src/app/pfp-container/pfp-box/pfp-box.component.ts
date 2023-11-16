44a63c5e344b5712048a20e27f164d10d2691d4d
BIDAUX Alexandre <ABX3@dsone.3ds.com>
2023-11-16 17:18:39 +0100

    Functionnal download

--- src/app/pfp-container/pfp-box/pfp-box.component.ts
+++ src/app/pfp-container/pfp-box/pfp-box.component.ts
@@ -1,19 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Pfp } from '../../shared/models/pfp.model';
 import { Size } from '../../shared/models/size.enum';
+import { getStorage, ref, getDownloadURL, getBlob } from "firebase/storage";
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+
 
 @Component({
   selector: 'app-pfp-box',
   templateUrl: './pfp-box.component.html',
   styleUrl: './pfp-box.component.scss'
 })
-export class PfpBoxComponent {
+export class PfpBoxComponent implements OnInit {
   @Input({ required: true }) pfp!: Pfp;
   @Input({ required: true }) size!: Size;
 
   showHuge: boolean = false;
 
-  download(event: Event) {
-    event.stopPropagation();
+  downloadUrl!: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    getBlob(ref(getStorage(), `pfps/${this.pfp.name}`))
+      .then((blob) => {
+        this.downloadUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+      })
+      .catch((error) => {
+        alert("Cannot find image")
+      });
   }
 } 
